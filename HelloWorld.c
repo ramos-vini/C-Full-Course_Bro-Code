94404d8ed048307d458a26bb728602fc77023b6f
ramos-vini <ramoscontrole@gmail.com>
2023-04-26 17:30:47 +0200

    Variables

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,8 +2,14 @@
 
 int main(){
 
-    printf("I like pizza!\n");
-    printf("It's really good");
+    int age = 21;
+    float gpa = 2.05;
+    char grade = 'C';
+    char name[] = "Bro";
 
+    printf("Hello, %s!\n", name);
+    printf("You are %d years old\n", age);
+    printf("Your average grade is %c\n", grade);
+    printf("And your gpa is %0.2f\n", gpa);
     return 0;
 }
