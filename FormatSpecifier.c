edd189641b0826d28cc9e41e630dd885a2b164cb
ramos-vini <ramoscontrole@gmail.com>
2023-05-03 22:35:18 +0200

    format specifiers

---
+++ FormatSpecifier.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    float item1 = 5.75;
+    float item2 = 10.0;
+    float item3 = 7.50;
+
+    printf("Item 1: $%.2f\n", item1);
+    printf("Item 2: $%.2f\n", item2);
+    printf("Item 3: $%.2f\n", item3);
+
+    return 0;
+}
