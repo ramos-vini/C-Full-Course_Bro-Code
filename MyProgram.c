6dac605e83b87e433a728868af6845a898774e99
ramos-vini <ramoscontrole@gmail.com>
2023-05-23 11:39:44 +0200

    Function Prototypes

--- MyProgram.c
+++ MyProgram.c
@@ -1,45 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 
+void hello(char[], int); // Function Prototype
+
 int main(){
 
-    double n1, n2;
-    char op;
-    double result;
-
-    printf("\nCalculator Program\n");
-    printf("\nType in the first number:\n");
-    scanf("%lf",&n1);
-
-    printf("Type in the operator sign ('+', '-', '*' or '/'):\n");
-    scanf("%s",&op);
-
-    printf("Type in the second number:\n");
-    scanf("%lf",&n2);
-
-    switch (op)
-    {
-    case '+':
-        result = n1 + n2;
-        break;
+    hello("Vini", 21);
     
-    case '-':
-        result = n1 - n2;
-        break;
-
-    case '*':
-        result = n1 * n2;
-        break;
-
-    case '/':
-        result = n1 / n2;
-        break;
-
-    default:
-        break;
-    }
-
-    printf("\n%lf %c %lf = %lf\n", n1, op, n2, result);
-
     return 0;
+}
+
+void hello(char name[], int age){
+    printf("\nHello %s, you are %d years old.\n", name, age);
 }
