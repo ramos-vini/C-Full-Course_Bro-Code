c95f444f7ebed9313e33a66b3898ab61b5b084d8
ramos-vini <ramoscontrole@gmail.com>
2023-05-27 22:20:30 +0200

    Enhanced Number Guessing Game

--- MyProgram.c
+++ MyProgram.c
@@ -6,20 +6,34 @@ int main()
 {
     srand(time(0));
 
-    int random= rand() % 10 + 1;
-    int number;
+    int answer = rand() % 100 + 1;
+    int guess;
+    int guesses = 0;
 
-    printf("Choose a number between 1-10:\n");
-    scanf("%d", &number);
+    printf("Number Guessing Game!\n\n");
 
-    printf("\nRandom Number: %d\n", random);
-    printf("Chosen Number: %d\n", number);
-    
-    if(number == random){
-        printf("\nCongratulations! You got it right :)");
-    } else{
-        printf("\nMaybe next time...");
-    }
+    do
+    {
+        printf("Pick a number between 1-100:");
+        scanf("%d", &guess);
+        guesses++;
+
+        if (guess > answer)
+        {
+            printf("Too high!\n");
+        }
+        else if (guess != answer)
+        {
+            printf("Too low!\n");
+        }
+
+    } while (guess != answer);
+
+    printf("\nCORRECT!\n");
+    printf("\n**************\n");
+    printf("Answer: %d\n", answer);
+    printf("Guesses: %d\n", guesses);
+    printf("**************");
 
     return 0;
 }
