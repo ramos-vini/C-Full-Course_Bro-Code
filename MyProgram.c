ec570f30dc2112ab5dea1126ff6e8ba7b97e443f
ramos-vini <ramoscontrole@gmail.com>
2023-05-27 21:55:11 +0200

    Number Guessing Game

--- MyProgram.c
+++ MyProgram.c
@@ -6,9 +6,20 @@ int main()
 {
     srand(time(0));
 
-    int random= rand() % 6 + 1;
+    int random= rand() % 10 + 1;
+    int number;
 
-    printf("%d", random);
+    printf("Choose a number between 1-10:\n");
+    scanf("%d", &number);
+
+    printf("\nRandom Number: %d\n", random);
+    printf("Chosen Number: %d\n", number);
+    
+    if(number == random){
+        printf("\nCongratulations! You got it right :)");
+    } else{
+        printf("\nMaybe next time...");
+    }
 
     return 0;
 }
